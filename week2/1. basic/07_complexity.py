984170784c4f7d00ea9b35b1a23d84fc277044a5
sihoo <pelomon73@gmail.com>
2026-08-29 19:08:42 +0900

    07-2

--- week2/1. basic/07_complexity.py
+++ week2/1. basic/07_complexity.py
@@ -36,6 +36,12 @@ def find_duplicates_brute_force(nums):
     ## 같은 원소를 찾으면 duplicates에 추가 (중복 추가 방지 필요)
     pass
     
+    for i in range(n):
+        for j in range(i+1, n):
+            if nums[i] == nums[j] and nums[i] not in duplicates:
+                duplicates.append(nums[i])
+
+
     return duplicates
 
 def find_duplicates_sorting(nums):
