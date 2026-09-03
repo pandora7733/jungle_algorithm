5c4ebd58a3cb97de5892683122dc9e2f47ac557b
sihoo <pelomon73@gmail.com>
2026-09-03 22:30:21 +0900

    week3 3

--- week3/1. basic/03_graph_basic.py
+++ week3/1. basic/03_graph_basic.py
@@ -42,11 +42,27 @@ def create_graph(vertices, edges, directed=False):
     """
     # TODO: 빈 그래프 초기화
     pass
+
+    # 그래프를 추가
+    graph = {}
+
+    for vertex in range(vertices):
+        graph[vertex] = []
     
     # TODO: 간선 추가
     ## 간선 추가 (u에서 v로)
     ## 무방향 그래프면 반대 방향도 추가
     pass
+
+    for u, v in edges:
+        # false는 무방향
+        if not directed:
+            graph[u].append(v)
+            graph[v].append(u)
+        # true는 방향
+        else:
+            graph[u].append(v)
+
     
     return graph
 
